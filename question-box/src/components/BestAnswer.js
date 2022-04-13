bf213bd06ccfa2c674aa19054bd5519856df1dab
Brittany Craig <brittanycraig@gmail.com>
2022-04-13 01:28:40 -0400

    more minor changes trying to fix best answer bug

--- question-box/src/components/BestAnswer.js
+++ question-box/src/components/BestAnswer.js
@@ -26,16 +26,19 @@ export default function BestAnswer({ token, responseId, questionId, acceptedResp
         }
       }
       ).then(res => {
+        setAcceptedExists(true)
         setBestAnswer(true)
         setBestQuestionPk(res.data.question)
         console.log(res)
         console.log("best answer pk:" + res.data.question)
         console.log(questionId)
+        console.log(acceptedExists)
 
       })
   }
 
-
+///how do I get "acceptedExists to set to true so I can use that?? I tried to do it on QuestionDetail also and it's not working
+//originally had a nested ternary in the return to set the selected answer here and somehow ended up with it now in the question detail, but it should go here, ideally
 
     return (
       
